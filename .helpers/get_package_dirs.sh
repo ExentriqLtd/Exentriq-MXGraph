4494bd4f11641e6594b5068664d8386e414f68fb
zhe1ka <zheka.dzecina@gmail.com>
2024-04-05 11:38:34 +0300

    removed unnecessary packages from "get_package_dirs.sh" file

--- .helpers/get_package_dirs.sh
+++ .helpers/get_package_dirs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 function join { local IFS="$1"; shift; echo "$*"; }
 
-packages=("meteor-easy-search/packages/" "Exentriq-MSP/packages/" "meteor-streamer/packages" "jalik-ufs/packages" "exentriq-packages/" "Meteor-CollectionFS/packages");
+packages=("Exentriq-MSP/packages/");
 packagesAbs=()
 for f in ${packages[@]}; do
     packagesAbs+=("$PWD/../$f")
